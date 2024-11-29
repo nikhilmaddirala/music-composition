9cd65cd22fe1f6edfcc8f86e707158113882b478
Nikhil Maddirala <nikhil.maddirala@gmail.com>
2024-11-29 02:03:16 +0000

    feat: Add glissando spanner between notes in scratch-2024-11-28.py

--- scratch-2024-11-28.py
+++ scratch-2024-11-28.py
@@ -6,8 +6,12 @@ us['directoryScratch'] = '/tmp'
 
 # Create a Stream
 s = converter.parse('tinyNotation: 4/4 C4 D4 E4 F4 G4 A4 B4 c4')
-
 s.insert(0, instrument.Violin())
+# Add glissando spanner between each pair of notes
+for i in range(len(s.notes) - 1):
+    gliss = spanner.Glissando(s.notes[i], s.notes[i + 1])
+    s.insert(0, gliss)
+    print(f"Added glissando from {s.notes[i]} to {s.notes[i + 1]}")
 
 # s.getInstruments().show('text')
 # # Ensure each note has the instrument set
@@ -15,13 +19,8 @@ s.insert(0, instrument.Violin())
 #     n.activeSite = None
 #     n.instrument = instrument.Violin()
 
-s.show()
-
+s.show('text')
 s.show('midi')
-
-# Play the stream using midi.realtime.StreamPlayer
-sp = midi.realtime.StreamPlayer(s)
-sp.play()
 
 def write_midi(stream):
     mf = midi.translate.music21ObjectToMidiFile(stream)
@@ -43,7 +42,6 @@ def print_midi_file_info(path):
             print(event)
 
 
-write_midi(s)
-print_midi_file_info('/workspaces/music-composition/<music21.stream.Part 0x7a2491d87bc0>.mid')
+# write_midi(s)
+# print_midi_file_info('/workspaces/music-composition/<music21.stream.Part 0x7a2491d87bc0>.mid')
 
-s.getInstrument()
